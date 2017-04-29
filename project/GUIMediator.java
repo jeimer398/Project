335d837400ef9e3027ee96152902d26a4f9fb26f
jeimer398 <josheimer98@gmail.com>
2017-04-28 20:51:53 -0400

    Part 8

--- project/GUIMediator.java
+++ project/GUIMediator.java
@@ -1,29 +1,188 @@
 package project;
 
 import javax.swing.*;
+import java.util.Observable;
 
-/**
- * Created by bengw on 4/28/2017.
- */
-public class GUIMediator {
+public class GUIMediator extends Observable{
+	private MachineModel model;
+	private FilesMgr filesMgr;
+	private StepControl stepControl;
+	private JFrame frame;
 
-    private MachineModel model;
+	public void step(){
+		if(model.getCurrentState() != States.PROGRAM_HALTED && model.getCurrentState() != States.NOTHING_LOADED){
+			try{
+				model.step();
+			}catch (CodeAccessException e) {
+				JOptionPane.showMessageDialog(
+						frame, 
+						"Illegal access to code from line " + model.getpCounter() + "\n"
+								+ "Exception message: " + e.getMessage(),
+								"Run time error",
+								JOptionPane.OK_OPTION);
+			}catch (ArrayIndexOutOfBoundsException e){
+				JOptionPane.showMessageDialog(
+						frame, 
+						"Illegal access to data" + "\n"
+								+ "Exception message: " + e.getMessage(),
+								"Run time error",
+								JOptionPane.OK_OPTION);
+			}catch (NullPointerException e){
+				JOptionPane.showMessageDialog(
+						frame,
+						"Illegal access to data" + "\n"
+								+ "Exception message: " + e.getMessage(),
+								"Run time error",
+								JOptionPane.OK_OPTION);
+			}catch (IllegalArgumentException e){
+				JOptionPane.showMessageDialog(
+						frame,
+						"Illegal access to code" + "\n"
+								+ "Exception message: " + e.getMessage(),
+								"Run time error",
+								JOptionPane.OK_OPTION);
+			}catch (DivideByZeroException e){
+				JOptionPane.showMessageDialog(
+						frame,
+						"Illegal access to code " + model.getpCounter() + "\n"
+						+ "Exception message: " + e.getMessage(),
+						"Divide by zero error",
+						JOptionPane.OK_OPTION);
+			}
+			setChanged();
+			notifyObservers();
+		}
+	}
 
-    public void step() {} //complete later
+	public void execute(){
+		while(model.getCurrentState() != States.PROGRAM_HALTED && model.getCurrentState() != States.NOTHING_LOADED){
+			try{
+				model.step();
+			}catch (CodeAccessException e) {
+				JOptionPane.showMessageDialog(
+						frame, 
+						"Illegal access to code from line " + model.getpCounter() + "\n"
+								+ "Exception message: " + e.getMessage(),
+								"Run time error",
+								JOptionPane.OK_OPTION);
+			}catch (ArrayIndexOutOfBoundsException e){
+				JOptionPane.showMessageDialog(
+						frame, 
+						"Illegal access to data" + "\n"
+								+ "Exception message: " + e.getMessage(),
+								"Run time error",
+								JOptionPane.OK_OPTION);
+			}catch (NullPointerException e){
+				JOptionPane.showMessageDialog(
+						frame,
+						"Illegal access to data" + "\n"
+								+ "Exception message: " + e.getMessage(),
+								"Run time error",
+								JOptionPane.OK_OPTION);
+			}catch (IllegalArgumentException e){
+				JOptionPane.showMessageDialog(
+						frame,
+						"Illegal access to code" + "\n"
+								+ "Exception message: " + e.getMessage(),
+								"Run time error",
+								JOptionPane.OK_OPTION);
+			}catch (DivideByZeroException e){
+				JOptionPane.showMessageDialog(
+						frame,
+						"Illegal access to code " + model.getpCounter() + "\n"
+						+ "Exception message: " + e.getMessage(),
+						"Divide by zero error",
+						JOptionPane.OK_OPTION);
+			}
+		}
+		setChanged();
+		notifyObservers();
+	}
+	
+	public JFrame getFrame() {
+		return frame;
+	}
 
-    public JFrame getFrame() {
-        return null; //temporary
-    }
+	public void clearJob() {
+		model.clearJob();
+		model.setCurrentState(States.NOTHING_LOADED);
+		model.getCurrentState().enter();
+		setChanged();
+		notifyObservers("Clear");
+	}
 
-    public void clearJob() {} //complete later
+	public void toggleAutoStep(){
+		stepControl.toggleAutoStep();
+		if(stepControl.isAutoStepOn()){
+			model.setCurrentState(States.AUTO_STEPPING);
+		}else{
+			model.setCurrentState(States.PROGRAM_LOADED_NOT_AUTOSTEPPING);
+		}
+		model.getCurrentState().enter();
+		setChanged();
+		notifyObservers();
+	}
 
-    public void makeReady(String s) {} //complete later
+	public void reload(){
+		stepControl.setAutoStepOn(false);
+		clearJob();
+		filesMgr.finalLoad_ReloadStep(model.getCurrentJob());
+	}
 
-    public MachineModel getModel() {
-        return model;
-    }
+	public void assembleFile(){
+		filesMgr.loadFile(model.getCurrentJob());
+	}
 
-    public void setModel(MachineModel model) {
-        this.model = model;
-    }
+	public void setPeriod(int period){
+		stepControl.setPeriod(period);
+	}
+
+	public void changeToJob(int i){
+		model.changeToJob(i);
+		if(model.getCurrentState()!=null){
+			model.getCurrentState().enter();
+			setChanged();
+			notifyObservers();
+		}
+	}
+
+	public void makeReady(String string){
+		stepControl.setAutoStepOn(false);
+		model.setCurrentState(States.PROGRAM_LOADED_NOT_AUTOSTEPPING);
+		model.getCurrentState().enter();
+		setChanged();
+		notifyObservers(string);
+	}
+
+	public MachineModel getModel() {
+		return model;
+	}
+
+	public void setModel(MachineModel model) {
+		this.model = model;
+	}
+
+	public States getCurrentState(){
+		return model.getCurrentState();
+	}
+
+	public void setCurrentState(States s){
+		if(s == States.PROGRAM_HALTED){
+			stepControl.setAutoStepOn(false);
+		}
+		model.setCurrentState(s);
+		//3 Notify Lines
+		model.getCurrentState().enter();
+		setChanged();
+		notifyObservers();
+	}
+
+	public void exit() { // method executed when user exits the program
+		int decision = JOptionPane.showConfirmDialog(
+				frame, "Do you really wish to exit?",
+				"Confirmation", JOptionPane.YES_NO_OPTION);
+		if (decision == JOptionPane.YES_OPTION) {
+			System.exit(0);
+		}
+	}
 }

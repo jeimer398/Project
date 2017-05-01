a9c123f1c788f3b23329ce3943841e15907281a8
Benjamin  Weiss <bengw99@gmail.com>
2017-04-30 21:44:46 -0400

    Part 6 tests pass 100%

--- project/MachineModel.java
+++ project/MachineModel.java
@@ -24,37 +24,40 @@ public class MachineModel {
 			jobs[i].setStartmemoryIndex(i*Memory.DATA_SIZE/4);
 		}
 		currentJob = jobs[0];
-		//NOP
+		//INSTRUCTION MAP entry for "NOP"
 		IMAP.put(0x0, (arg, level) -> {
 			cpu.incrPC();
 		});
-		//LOD
+
+		//INSTRUCTION MAP entry for "LOD"
 		IMAP.put(0x1, (arg, level) -> {
-			if(level < 0 || level > 2){
-				throw new IllegalArgumentException("Illegal indirection level in LOD instruction.");
+			if(level < 0 || level > 2) {
+				throw new IllegalArgumentException(
+						"Illegal indirection level in LOD instruction");
 			}
-			if(level>0){
-				IMAP.get(0x1).execute(memory.getData(cpu.getMemBase()), level-1);
-			}
-			else{
+			if(level > 0) {
+				IMAP.get(0x1).execute(memory.getData(cpu.getMemBase()+arg), level-1);
+			} else {
 				cpu.setAccum(arg);
 				cpu.incrPC();
 			}
 		});
-		//STO
+
+		//INSTRUCTION MAP entry for "STO"
 		IMAP.put(0x2, (arg, level) -> {
-			if(level < 1 || level > 2){
-				throw new IllegalArgumentException("Illegal indirection level in STO instruction.");
+			if(level < 1 || level > 2) {
+				throw new IllegalArgumentException(
+						"Illegal indirection level in STO instruction");
 			}
-			if(level == 1){
-				memory.setData(arg,  cpu.getAccum());
+			if(level > 1) {
+				IMAP.get(0x2).execute(memory.getData(cpu.getMemBase()+arg), level-1);
+			} else {
+				memory.setData(cpu.getMemBase() + arg, cpu.getAccum());
 				cpu.incrPC();
 			}
-			else {
-				IMAP.get(0x2).execute(memory.getData(cpu.getMemBase()), level-1);
-			}
 		});
-		//ADD
+
+		//INSTRUCTION MAP entry for "ADD"
 		IMAP.put(0x3, (arg, level) -> {
 			if(level < 0 || level > 2) {
 				throw new IllegalArgumentException(
@@ -67,51 +70,62 @@ public class MachineModel {
 				cpu.incrPC();
 			}
 		});
-		//SUB
+
+		//INSTRUCTION MAP entry for "SUB"
 		IMAP.put(0x4, (arg, level) -> {
 			if(level < 0 || level > 2) {
 				throw new IllegalArgumentException(
 						"Illegal indirection level in SUB instruction");
 			}
 			if(level > 0) {
-				IMAP.get(0x3).execute(memory.getData(cpu.getMemBase()+arg), level-1);
+				IMAP.get(0x4).execute(memory.getData(cpu.getMemBase()+arg), level-1);
 			} else {
 				cpu.setAccum(cpu.getAccum() - arg);
 				cpu.incrPC();
 			}
 		});
-		//MUL
-		IMAP.put(0x5, (arg, level) ->{
+
+		//INSTRUCTION MAP entry for "MUL"
+		IMAP.put(0x5, (arg, level) -> {
 			if(level < 0 || level > 2) {
 				throw new IllegalArgumentException(
-						"Illegal indirection level in MUL instruction.");}
+						"Illegal indirection level in MUL instruction");
+			}
 			if(level > 0) {
-				IMAP.get(0x3).execute(memory.getData(cpu.getMemBase()+arg), level-1);
+				IMAP.get(0x5).execute(memory.getData(cpu.getMemBase()+arg), level-1);
 			} else {
 				cpu.setAccum(cpu.getAccum() * arg);
-				cpu.incrPC();}});
+				cpu.incrPC();
+			}
+		});
 
-		//DIV
-		IMAP.put(0x6, (arg, level) ->{
+		//INSTRUCTION MAP entry for "DIV"
+		IMAP.put(0x6, (arg, level) -> {
 			if(level < 0 || level > 2) {
 				throw new IllegalArgumentException(
-						"Illegal indirection level in DIV instruction.");}
-			if(level > 0) {
-				IMAP.get(0x3).execute(memory.getData(cpu.getMemBase()+arg), level-1);} 
-			if(arg==0)
-				throw new DivideByZeroException("Can't divide by zero.");
-			else {
-				cpu.setAccum(cpu.getAccum() / arg);
-				cpu.incrPC();}});
-		//AND
-		IMAP.put(0x7, (arg, level) -> {
-			if(level < 0 || level > 1){
-				throw new IllegalArgumentException("Illegal indirection level in AND instruction.");
+						"Illegal indirection level in DIV instruction");
 			}
-			if (level > 0) {
-				IMAP.get(0x7).execute(memory.getData(arg), level-1);
+			if(level > 0) {
+				IMAP.get(0x6).execute(memory.getData(cpu.getMemBase()+arg), level-1);
 			} else {
-				if (arg != 0 && cpu.getAccum() != 0) {
+				if (arg == 0) {
+					throw new DivideByZeroException();
+				}
+				cpu.setAccum(cpu.getAccum() / arg);
+				cpu.incrPC();
+			}
+		});
+
+		//INSTRUCTION MAP entry for "AND"
+		IMAP.put(0x7, (arg, level) -> {
+			if(level < 0 || level > 2) {
+				throw new IllegalArgumentException(
+						"Illegal indirection level in AND instruction");
+			}
+			if(level > 0) {
+				IMAP.get(0x7).execute(memory.getData(cpu.getMemBase()+arg), level-1);
+			} else {
+				if (cpu.getAccum() != 0 && arg != 0) {
 					cpu.setAccum(1);
 				}
 				else {
@@ -120,43 +134,56 @@ public class MachineModel {
 				cpu.incrPC();
 			}
 		});
-		//NOT
-		IMAP.put(0x8, (arg,level) -> {
-			if(cpu.getAccum() == 0){
-				cpu.setAccum(1);
+
+		//INSTRUCTION MAP entry for "NOT"
+		IMAP.put(0x8, (arg, level) -> {
+			if(level != 0){
+				throw new IllegalArgumentException(
+						"Illegal indirection level in NOT instruction");
+			}
+			if (cpu.getAccum() != 0) {
+				cpu.setAccum(0);
 			}
 			else {
-				cpu.setAccum(0);
+				cpu.setAccum(1);
 			}
 			cpu.incrPC();
 		});
-		//CMPL
-		IMAP.put(0x9, (arg,level) -> {
-			if(level != 1) {
+
+		//INSTRUCTION MAP entry for "CMPL"
+		IMAP.put(0x9, (arg, level) -> {
+			if(level < 1 || level > 2) {
 				throw new IllegalArgumentException(
-						"Illegal indirection level in CMPL instruction.");
+						"Illegal indirection level in CMPL instruction");
 			}
-			if(memory.getData(arg) < 0){
-				cpu.setAccum(1);
+			if(level > 1) {
+				IMAP.get(0x9).execute(memory.getData(cpu.getMemBase()+arg), level-1);
+			} else {
+				if(memory.getData(cpu.getMemBase()+arg) < 0) {
+					cpu.setAccum(1);
+				} else {
+					cpu.setAccum(0);
+				}
+				cpu.incrPC();
 			}
-			else{
-				cpu.setAccum(0);
-			}
-			cpu.incrPC();
 		});
-		//CMPZ
-		IMAP.put(0xA, (arg,level) -> {
-			if(level != 1) {
+
+		//INSTRUCTION MAP entry for "CMPZ"
+		IMAP.put(0xA, (arg, level) -> {
+			if(level < 1 || level > 2) {
 				throw new IllegalArgumentException(
-						"Illegal indirection level in CMPZ instruction.");
+						"Illegal indirection level in CMPZ instruction");
 			}
-			if(memory.getData(arg) == 0){
-				cpu.setAccum(1);
+			if(level > 1) {
+				IMAP.get(0xA).execute(memory.getData(cpu.getMemBase()+arg), level-1);
+			} else {
+				if(memory.getData(cpu.getMemBase()+arg) == 0) {
+					cpu.setAccum(1);
+				} else {
+					cpu.setAccum(0);
+				}
+				cpu.incrPC();
 			}
-			else{
-				cpu.setAccum(0);
-			}
-			cpu.incrPC();
 		});
 		//INSTRUCTION MAP entry for "JUMP"
 		IMAP.put(0xB, (arg, level) -> {
@@ -168,14 +195,14 @@ public class MachineModel {
 				int arg1 = memory.getData(cpu.getMemBase()+arg); // CORRECTION
 				cpu.setpCounter(arg1 + currentJob.getStartcodeIndex());   // CORRECTION
 			} else if(level > 0) {
-				IMAP.get(0x2).execute(memory.getData(cpu.getMemBase()+arg), level-1);
+				IMAP.get(0xB).execute(memory.getData(cpu.getMemBase()+arg), level-1);
 			} else {
 				cpu.setpCounter(cpu.getpCounter() + arg);
 			}
 		});
 
 		//INSTRUCTION MAP entry for "JUMP"
-		IMAP.put(0xB, (arg, level) -> {
+		IMAP.put(0xC, (arg, level) -> {
 			if(level < 0 || level > 3) {
 				throw new IllegalArgumentException(
 						"Illegal indirection level in JUMP instruction");
@@ -185,7 +212,7 @@ public class MachineModel {
 					int arg1 = memory.getData(cpu.getMemBase()+arg); // CORRECTION
 					cpu.setpCounter(arg1 + currentJob.getStartcodeIndex());   // CORRECTION
 				} else if (level > 0) {
-					IMAP.get(0x2).execute(memory.getData(cpu.getMemBase() + arg), level - 1);
+					IMAP.get(0xC).execute(memory.getData(cpu.getMemBase() + arg), level - 1);
 				} else {
 					cpu.setpCounter(cpu.getpCounter() + arg);
 				}
